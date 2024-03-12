dcc63141eba0519096422eb8d622b2fb7f3566c1
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-03-12 16:59:35 -0500

    remove stupid dumb build files

--- Assets/bug-characters/BugController.cs
+++ Assets/bug-characters/BugController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class dungBeetleController : MonoBehaviour
+public class BugController : MonoBehaviour
 {
     public float speed = 5.0f;
     private Vector2 moveDirection;
@@ -11,6 +11,7 @@ public class dungBeetleController : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         moveDirection = context.ReadValue<Vector2>();
+
     }
     // Start is called before the first frame update
     void Start()
@@ -21,14 +22,18 @@ public class dungBeetleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveDung();
+        moveBug();
     }
 
-    void moveDung(){
+    // actually moves the bug with rotation
+    void moveBug(){
         Vector3 movement = new Vector3(moveDirection.x, 0.0f, moveDirection.y);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+        if(moveDirection != Vector2.zero){
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+        }
 
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
+
 }
