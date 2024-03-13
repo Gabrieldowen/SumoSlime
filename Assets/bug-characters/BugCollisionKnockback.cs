dfbe98e32f170671a1f66d77e4998c7ef45dde31
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-03-13 10:26:44 -0500

    add knockback script and add to dungBeetle in flat map

---
+++ Assets/bug-characters/BugCollisionKnockback.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionKnockback : MonoBehaviour
+{
+    [SerializeField] private float knockbackForce;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 direction = collision.transform.position - transform.position;
+            direction.y = 0;
+            rb.AddForce(direction.normalized * knockbackForce, ForceMode.Impulse);
+        }
+    }
+}
