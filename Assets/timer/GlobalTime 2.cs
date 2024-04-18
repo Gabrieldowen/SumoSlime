a63d4007e643b62b92069c9512d29df806f4df74
Blake Reynolds <blakereynolds2001@gmail.com>
2024-04-18 16:02:16 -0500

    add time?
    I thought I already added this last week, so I'm not sure why this is popped up

---
+++ Assets/timer/GlobalTime 2.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GlobalTime : MonoBehaviour
+{
+    private float timer = 10f;
+    private TextMeshProUGUI timerSeconds;
+
+
+    void Start()
+    {
+        timerSeconds = GetComponent<TextMeshProUGUI>();
+    }
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f1");
+        if (timer <= 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+
+    //public GameObject timeDisplay;
+    //public int seconds = 30;
+    //public bool deductTime;
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (deductTime == false)
+    //    {
+    //        deductTime = true;
+    //        StartCoroutine(deductSecond());
+    //    }
+    //}
+
+    //IEnumerator deductSecond()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //    seconds -= 1;
+    //    timeDisplay.GetComponent<Text>().text = "TIME: " + seconds;
+    //    deductTime = false;
+    //}
+
+}
