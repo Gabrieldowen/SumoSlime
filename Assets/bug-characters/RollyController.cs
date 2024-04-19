5472ebd32d24691590a593949caa83ba52aa88a8
Gabriel <Gabriel.d.owen-1@ou.edu>
2024-04-19 15:57:58 -0500

    improve collision

--- Assets/bug-characters/RollyController.cs
+++ Assets/bug-characters/RollyController.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class BugController : MonoBehaviour
+public class RollyController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    [SerializeField] private float speed = 5.0f;
     private Vector2 moveDirection;
+    private Collider playerCollider;
 
     private Rigidbody playerRB;
     private Vector3 startPOS;
+    private bool isMoving = false;
+
+    public float rollSpeed = 3.0f;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveDirection = context.ReadValue<Vector2>();
-
+        playerCollider = GetComponent<Collider>(); // Assuming the player has a collider component
     }
     // Start is called before the first frame update
     void Start()
@@ -29,6 +33,8 @@ public class BugController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isMoving)
+            return;
         moveBug();
     }
 
@@ -37,12 +43,35 @@ public class BugController : MonoBehaviour
         // moves bug
         Vector3 movement = new Vector3(moveDirection.x, 0.0f, moveDirection.y);
 
-        // rotates bug
+        //rotates bug
         if(moveDirection != Vector2.zero){
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F); 
+   
         }
 
+        //actually moves the bug
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        // // roll the bug
+        // if(moveDirection != Vector2.zero){
+
+        //      var anchor = playerCollider.bounds.center;
+        //      var axis = Vector3.Cross(Vector3.up, movement);
+
+        //      print("\npos "+transform.position+"anchor " + anchor + "axis " + axis);
+
+        //      // StartCoroutine(rollBug(anchor, axis));
+        //  }
+    }
+
+    IEnumerator rollBug(Vector3 anchor, Vector3 axis){
+        isMoving = true;
+        for(int i = 0; i<(90/rollSpeed); i++){
+            transform.RotateAround(anchor, axis, 3);
+            yield return new WaitForSeconds(0.1f);
+        }
+        isMoving = false;
     }
 
     void OnTriggerEnter(Collider other){
@@ -52,14 +81,13 @@ public class BugController : MonoBehaviour
             resetGameState();
             // here you can reset the trail or update count of falling/deaths
         }
-        if(other.tag == "Ground" && playerRB.velocity != Vector3.zero && playerRB.angularVelocity != Vector3.zero){
+        if(other.tag == "Ground"){
             // Nullify the force application when grounded
             playerRB.velocity = Vector3.zero;
             playerRB.angularVelocity = Vector3.zero;
             return; // Exit the Update method early to prevent applying additional force
 
         }
-
     }
 
     void resetGameState(){
