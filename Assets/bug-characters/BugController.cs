df2a2d10bea5e8b5ad641c05a76ccda715752b44
Dennis J Case JR <denniscase1999@gmail.com>
2024-04-20 20:22:29 -0500

    Added a slime trail
    Have a working slime trail, code is inside the BugController.cs file
    
    Rollie Pollie is now not moving but ladybug is (arrows work, WASD do not,
    idk why)

--- Assets/bug-characters/BugController.cs
+++ Assets/bug-characters/BugController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
+using UnityEngine.Tilemaps;
 
 public class BugController : MonoBehaviour
 {
@@ -14,6 +15,13 @@ public class BugController : MonoBehaviour
 
     private Rigidbody playerRB;
     private Vector3 startPOS;
+    [Header("Slime Types")]
+    [SerializeField] private GameObject slimePrefab;
+
+    [Header("Spawn System")]
+    [SerializeField] private float spawnTimer = 0f;
+    public Tilemap tileMap = null;
+    //public List<Vector3> availablePlaces;
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -28,12 +36,42 @@ public class BugController : MonoBehaviour
         startPOS = playerRB.position;
         print("start pos is set to: " + startPOS);
 
+        tileMap = transform.parent.GetComponent<Tilemap>();
+        /*availablePlaces = new List<Vector3>();
+
+        // Iterate through the tilemap cells
+        for (int n = tileMap.cellBounds.xMin; n < tileMap.cellBounds.xMax; n++)
+        {
+            for (int p = tileMap.cellBounds.yMin; p < tileMap.cellBounds.yMax; p++)
+            {
+                Vector3Int localPlace = new Vector3Int(n, p, (int)tileMap.transform.position.y);
+                Vector3 place = tileMap.CellToWorld(localPlace);
+
+                if (tileMap.HasTile(localPlace))
+                {
+                    // Tile at "place" is available for spawning
+                    availablePlaces.Add(place);
+                }
+                else
+                {
+                    // No tile at "place"
+                }
+            }
+        }
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
+        spawnTimer += Time.deltaTime;
         moveBug();
+    }
+
+    private void FixedUpdate()
+    {
+        // Spawn slimes periodically
+        InvokeRepeating("SpawnSlime", spawnTimer, 0.05f);
     }
 
     // actually moves the bug with rotation
@@ -80,7 +118,20 @@ public class BugController : MonoBehaviour
         if(lives <= 0){            
             // go to the game over screen
             SceneManager.LoadScene("EndGame");
-            
+        }
+    }
+    private void SpawnSlime()
+    {
+        if (spawnTimer > 0.25)
+        {
+            // Instantiate a slime at a random available position
+            //int randomIndex = Random.Range(0, availablePlaces.Count);
+            //Vector3 spawnPosition = availablePlaces[randomIndex];
+            // Simulate character movement (you can replace this with your actual movement logic)
+            Vector3 characterPosition = transform.position;
+            Instantiate(slimePrefab, characterPosition, Quaternion.identity);
+
+            spawnTimer = 0f;
         }
     }
 
