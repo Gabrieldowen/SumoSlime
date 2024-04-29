59d977dc5dfa9a32227cc0bdc06c3a23ca7704f5
Blake Reynolds <blakereynolds2001@gmail.com>
2024-04-29 14:16:36 -0500

    Add Game Winner at end game screen

--- Assets/EndGameMenu.cs
+++ Assets/EndGameMenu.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class EndGameMenu : MonoBehaviour
 {
-    public void MainMenu() {
-        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex) -2);
+
+    public TextMeshProUGUI winnerText; // Assign this in the inspector
+
+    void Start()
+    {
+        if (UIManager.Instance != null)
+        {
+            string winner = UIManager.Instance.GetWinner();
+            if (winnerText != null)
+                winnerText.text = "Winner: " + winner;
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex) - 2);
     }
 
 
