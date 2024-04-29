314af9ae7e84294968ebd4c84a6b165e44c0ed96
Gabe <Gabriel.d.owen-1@ou.edu>
2024-04-29 17:16:31 -0500

    tweaks for 2 players

--- Assets/Scripts/GameSetup.cs
+++ Assets/Scripts/GameSetup.cs
@@ -5,6 +5,9 @@ public class GameSetup : MonoBehaviour
     public GameObject realDungBeetlePrefab;
     public GameObject rolliePrefab;
     public GameObject ladybug2Prefab;
+    public GameObject P2_RealDungBeetlePrefab;
+    public GameObject P2_rolliePrefab;
+    public GameObject P2_Ladybug2Prefab;
 
     private Vector3[] startPositions = new Vector3[]
     {
@@ -18,9 +21,9 @@ public class GameSetup : MonoBehaviour
         // Instantiate the selected characters at their start positions
         for (int i = 0; i < GameManager.Instance.selectedCharacters.Length; i++)
         {
-            string characterName = GameManager.Instance.selectedCharacters[i];
+            string characterName = GameManager.Instance.selectedCharacters[i] + (i+1);
             print("getting prefab for " + characterName + "...");
-            GameObject prefab = GetPrefabByName(characterName);
+            GameObject prefab = GetPrefabByName(characterName );
 
             // Set the player ID
             prefab.GetComponent<BugController>().playerID = i + 1;
@@ -37,13 +40,20 @@ public class GameSetup : MonoBehaviour
     {
         switch (characterName)
         {
-            case "DungBeetle":
+            case "DungBeetle1":
                 return realDungBeetlePrefab;
-            case "RolyPoly":
+            case "RolyPoly1":
                 return rolliePrefab;
-            case "LadyBug":
+            case "LadyBug1":
                 return ladybug2Prefab;
+            case "DungBeetle2":
+                return P2_RealDungBeetlePrefab;
+            case "RolyPoly2":
+                return P2_rolliePrefab;
+            case "LadyBug2":
+                return P2_Ladybug2Prefab;
             default:
+                Debug.LogError("CUSTOM ERROR Character name " + characterName + " is not recognized.");
                 return null;
         }
     }
