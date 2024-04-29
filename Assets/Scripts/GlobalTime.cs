f224b84a00010b1ab4665d4ebb27a0a978bca59b
Gabe <Gabriel.d.owen-1@ou.edu>
2024-04-29 15:06:35 -0500

    Revert "part of previous commit"
    This reverts commit 20f5b2f409f07dc2ac51daa4de7f03b46e272380.

--- Assets/Scripts/GlobalTime.cs
+++ Assets/Scripts/GlobalTime.cs
@@ -9,6 +9,7 @@ public class GlobalTime : MonoBehaviour
 {
     private float timer = 10f;
     private TextMeshProUGUI timerSeconds;
+    public string winnerText;
 
 
     void Start()
@@ -23,6 +24,32 @@ public class GlobalTime : MonoBehaviour
         timerSeconds.text = timer.ToString("f1");
 
         if (timer <= 0) {
+
+            BugController[] bugControllers = FindObjectsOfType<BugController>();
+            foreach (BugController bugController in bugControllers)
+            {
+                bugController.SaveScore();
+            }
+
+            int player1Score = PlayerPrefs.GetInt("Player_1_Score", 0);
+            int player2Score = PlayerPrefs.GetInt("Player_2_Score", 0);
+            if(player1Score > player2Score)
+            {
+                print("Player 1 Wins!: " + player1Score + " - " + player2Score);
+                winnerText = "Player 1 Wins!" + player1Score + " - " + player2Score;
+            }
+            else if(player2Score > player1Score)
+            {
+                print("Player 2 Wins!: "+ player1Score + " - " + player2Score);
+                winnerText = "Player 2 Wins!" + player2Score + " - " + player1Score;
+            }
+            else
+            {
+                print("Tie: " + player1Score + " - " + player2Score);
+                winnerText = "Tie!" + player1Score + " - " + player2Score;
+    
+            }
+            
 
             SceneManager.LoadScene("EndGame");
         }
